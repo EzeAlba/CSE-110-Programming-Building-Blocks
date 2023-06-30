0b8a9bc83957dd2338c9a5197d2884418be66ad5
EzeAlba <ezealba2@gmail.com>
2023-06-29 22:22:41 -0300

    23-6

--- 11_week/11_team_activity.py
+++ 11_week/11_team_activity.py
@@ -3,4 +3,7 @@ hr_system_file = []
 with open(r"C:\Users\Spread\Documents\Eze Code\CSE 110 Programming Building Blocks\11_week\hr_system.txt") as hr_system_file:
     for line in hr_system_file:
         hr_system_file = line.split(' ')
-        print(f'Name: {hr_system_file[0]}, Title: {hr_system_file[2]} ')
+        salary = (float(hr_system_file[3])/12)/2
+        if hr_system_file[2] == 'Engineer':
+            salary = salary + 1000.00
+        print(f'{hr_system_file[0]} (ID: {hr_system_file[1]}), {hr_system_file[2]} - ${salary:.2f}')
